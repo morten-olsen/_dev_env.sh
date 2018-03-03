a06cddffe92116c7548c5665a51bdecbe365a0fb
Morten Olsen <morten@olsen.pro>
2018-03-03 21:18:37 +0100

    Fixed a typo

--- magicinstall.sh
+++ magicinstall.sh
@@ -2,7 +2,7 @@ curl -o $PWD/magicenv.sh -L https://raw.githubusercontent.com/morten-olsen/magic
 if [ ! -f $PWD/magicenv.config.sh ]; then
   curl -o $PWD/magicenv.config.sh -L https://raw.githubusercontent.com/morten-olsen/magicenv/master/magicenv.config.template.sh
 fi
-curl -o $PWD/magicenv.ms -L https://github.com/morten-olsen/magicenv/blob/master/README.md
+curl -o $PWD/magicenv.md -L https://github.com/morten-olsen/magicenv/blob/master/README.md
 chmod +x $PWD/magicenv.sh
 rm -rf .magicenv
 echo "Done"
